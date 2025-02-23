3a172f3bd9a6ee82fb0b4c98109946f66ce9ef43
PedroAvila <minimainor_austin@hotmail.com>
2025-02-23 18:40:10 -0500

    Añadir registros de consola para las variables de entorno AZURE_SERVICE_BUS_CONNECTION_STRING y KEY_VAULT_URL

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import * as dotenv from 'dotenv';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 dotenv.config();
+console.log('AZURE_SERVICE_BUS_CONNECTION_STRING:', process.env.AZURE_SERVICE_BUS_CONNECTION_STRING);
+console.log('KEY_VAULT_URL:', process.env.KEY_VAULT_URL);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
